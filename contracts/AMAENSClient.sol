9c74b375e807e6b4bd521b91925feaea889edac8
graphicaldot <houzier.saurav@gmail.com>
2021-11-08 13:17:21 +0530

    Registration completed with refactord code but now the AMACLCLient has to encode the different label into _bytes for registration, if the domain is not available

--- contracts/AMAENSClient.sol
+++ contracts/AMAENSClient.sol
@@ -21,65 +21,121 @@ contract AMAENSClient is Controllable {
     string constant public BOOL_FALSE_STRING = "False";
     string constant public BOOL_TRUE_STRING = "True";
     
-    BaseRegistrarImplementation base;
+    address baseAddr;
     address resolver;
     uint duration;
     event NameRegistered(string name, bytes32 indexed label, address indexed owner, uint expires);
     event NameRenewed(string name, bytes32 indexed label,  uint expires);
 
-    constructor(BaseRegistrarImplementation _base, address _resolver, uint _duration){
-                base = _base;
+    constructor(address _baseRegistrarImplementation, address _resolver, uint _duration){
+                baseAddr = _baseRegistrarImplementation;
                 resolver = _resolver;
                 duration = _duration;
 
     }
 
-    
-    function register(string memory name, address owner, string memory twitterUsername, bytes memory _bytes) public onlyController {
-
-        (,string memory nameOnTwitter, string memory profileImage, string memory twitterID, bool isTwitterVerified, ) =  decode_data(_bytes);
-        bytes32 label = keccak256(bytes(name));
+    /**
+     * @dev Create a nodehash from the label with the help of the Namehash of the root domain.
+     * @param _label The _label for the domain like test.amafans, test is the label.
+    */
+    function getNodeHash(string memory _label) external view returns (bytes32,bytes32,uint256){
+        bytes32 label = keccak256(bytes(_label));
         uint256 tokenId = uint256(label);
+        return (label, keccak256(abi.encodePacked(BaseRegistrarImplementation(baseAddr).baseNode(), label)), tokenId);
+
+    }
     
+    /**
+     * @dev Create a nodehash from the label with the help of the Namehash of the root domain.
+     * @param _nodehash Nomehash of the full domain like Namehash(test.amafans).
+     * @param isTwitterVerified if the user has blue tick on the twitter profile or not.
+     * @param nameOnTwitter Name of the person as he puts it on the twitter profile.
+     * @param twitterUsername The twitter username of the person present on the twitter profile
+     * @param profileImage The url of the profileImage on the twitter account, this will be empty string if none is available
+     * @param twitterID this is the only thing unique, unchanged for a twitter account since it is internal to twitter
+    */
+    function _setKeyPairs(bytes32 _nodehash,
+                        bool isTwitterVerified,
+                        string memory nameOnTwitter,
+                        string memory twitterUsername,
+                        string memory profileImage,
+                        string memory twitterID
+                    ) 
+                    private {
+        bytes[] memory multiCallData = _setTextMulticall(_nodehash, isTwitterVerified, nameOnTwitter, twitterUsername, profileImage, twitterID);
+        Resolver(resolver).multicall(multiCallData);
+    }
 
-        uint expires;
-        // Set this contract as the (temporary) owner, giving it
-        // permission to set up the resolver.
-        expires = base.register(tokenId, address(this), duration);
+    /**
+    * @dev Transfers ownership of a node to a new address. May only be called by the current owner of the node.
+     * @param tokenId The node to transfer ownership of.
+     * @param owner The address of the new owner.
+    */
+    function _transferOwnership(uint256 tokenId, address owner) private {
+        BaseRegistrarImplementation(baseAddr).reclaim(tokenId, owner);
+        BaseRegistrarImplementation(baseAddr).transferFrom(address(this), owner, tokenId);
+    }
 
+
+    function registerNode(address _owner, 
+                bytes memory _bytes,
+                string memory twitterUsername)
+                external 
+                onlyController
+                returns (bytes32, string memory){
+            (string memory subdomain, 
+            string memory nameOnTwitter, 
+            string memory profileImage, 
+            string memory twitterID, 
+            bool isTwitterVerified, ) =  _decode_data(_bytes);
+
+        // bytes32 label = keccak256(bytes(_label));
+        // uint256 tokenId = uint256(label);
+    
         // The nodehash of this label
-        bytes32 nodehash = keccak256(abi.encodePacked(base.baseNode(), label));
+        (bytes32 label, bytes32 nodehash, uint256 tokenId) = this.getNodeHash(subdomain);
+        //check if the tokenId is available or nor
+        if (!BaseRegistrarImplementation(baseAddr).available(tokenId)){
+            revert("This domain is not available");
+        }
+        uint256 expires = _register(nodehash, tokenId);
 
-        //set key value pairs here
-        // Set the resolver
-        base.ens().setResolver(nodehash, resolver);
-        base.ens().setTTL(nodehash, uint64(expires));
 
 
         // Configure the resolver
-        Resolver(resolver).setAddr(nodehash, owner);
-        bytes[] memory multiCallData = _setTextMulticall(nodehash, isTwitterVerified, nameOnTwitter, twitterUsername, profileImage, twitterID);
-        Resolver(resolver).multicall(multiCallData);
+        // Resolver(resolver).setAddr(nodehash, _owner);
+        _setKeyPairs(nodehash, isTwitterVerified, nameOnTwitter, twitterUsername, profileImage, twitterID);
 
         // Now transfer full ownership to the expeceted owner
-        base.reclaim(tokenId, owner);
-        base.transferFrom(address(this), owner, tokenId);
+        _transferOwnership(tokenId, _owner);
+
+        emit NameRegistered(subdomain, label, _owner, expires);
+        return (nodehash, subdomain);
+    }
     
+    
+    function _register(bytes32 nodehash, 
+                uint256 tokenId)
+                private 
+                returns(uint256){
+         uint256 expires =BaseRegistrarImplementation(baseAddr).register(tokenId, address(this), duration);
 
-        emit NameRegistered(name, label, owner, expires);
+        //set key value pairs here
+        // Set the resolver
+        BaseRegistrarImplementation(baseAddr).ens().setResolver(nodehash, resolver);
+        BaseRegistrarImplementation(baseAddr).ens().setTTL(nodehash, uint64(expires));
+        return expires;
+    }
 
-
-        }
         
-        function renew(string calldata name, uint _duration) external payable onlyController {
+    function renew(string calldata name, uint _duration) external onlyController {
 
             bytes32 label = keccak256(bytes(name));
-            uint expires = base.renew(uint256(label), _duration);
-    
+            uint expires = BaseRegistrarImplementation(baseAddr).renew(uint256(label), _duration);
             emit NameRenewed(name, label, expires);
     }
         
-    function decode_data(bytes memory _bytes) private pure returns (string memory, string memory, string memory, string memory, bool, bool) {
+    function _decode_data(bytes memory _bytes) private pure returns (string memory, string memory, string memory, string memory, bool, bool) {
         (string memory username, 
         string memory name, 
         string memory profile_image,
@@ -90,6 +146,15 @@ contract AMAENSClient is Controllable {
         return (username, name, profile_image, id, twitter_verified, verified);
     }
     
+    // function _recordExists(bytes32 _nodehash, address _address) private view {
+    //     if (BaseRegistrarImplementation(baseAddr).ens().recordExists(_nodehash)){
+    //         revert("This subdomain already exists, You can now choose a different username for this address");
+    //     }
+    //     if (BaseRegistrarImplementation(baseAddr).ens().owner(_nodehash) == _address){
+    //         revert("You have already claimed");
+
+    //     }
+    //     }
     
         //https://github.com/ensdomains/ens-contracts/blob/master/contracts/root/Root.sol
     function _setTextMulticall(bytes32 nameHash, 
